5cf0c887048a1424da25d65d653818cb6dfc3d32
Martin Zlámal <mrtnzlml@gmail.com>
2014-11-25 19:54:21 +0100

    tests improvement, added Travis-CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,10 @@
 <?php
 
-require __DIR__ . '/../libs/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
 	exit(1);
-}
-
-if (extension_loaded('xdebug')) {
-	xdebug_disable();
-	\Tester\CodeCoverage\Collector::start(__DIR__ . '/coverage.dat');
 }
 
 Tester\Environment::setup();
